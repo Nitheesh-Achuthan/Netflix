bbb7f3e19645f694df8508082978c8dd2feeb9da
Nitheesh-Achuthan <nitheeshachuthan05@gmail.com>
2022-10-25 12:52:21 +0530

    design

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div banner_buttons>
+                <button className='button'>Play</button>
+                <button className='button'>My list</button>
+            </div>
+            <h1 className='description'>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk </h1>
+        </div>
+     <div className="fade_bottom"></div>
+    </div>
+  )
+}
+
+export default Banner
