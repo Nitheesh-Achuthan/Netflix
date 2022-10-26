48b7870b1cd97f8478f949eaf8cbadba1c4180b3
Nitheesh-Achuthan <nitheeshachuthan05@gmail.com>
2022-10-26 13:38:26 +0530

    netflix clone

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,17 +1,28 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { API_KEY,imageUrl } from '../../Constants/Constants'
+import axios from '../../Axios'
 import './Banner.css'
 
 function Banner() {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      setMovie(response.data.results[9])
+    })
+  }, [])
+  
   return (
-    <div className='banner'>
+    <div style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+    className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie ? movie.title : ''}</h1>
             <div banner_buttons>
                 <button className='button'>Play</button>
                 <button className='button'>My list</button>
             </div>
-            <h1 className='description'>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk </h1>
+            <h1 className='description'>{movie ? movie.overview : ''}</h1>
         </div>
      <div className="fade_bottom"></div>
     </div>
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "4655e7ae67badb5deb8e6ab10e897fe8";
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
